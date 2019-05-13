85157d4a74e217c23961e014a24cd1de10b30888
liach <liach@users.noreply.github.com>
2019-05-12 22:08:47 -0700

    Initialize
    Signed-off-by: liach <liach@users.noreply.github.com>

---
+++ src/main/java/com/github/liachmodded/doublecart/DoubleCartEntry.java
@@ -0,0 +1,16 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package com.github.liachmodded.doublecart;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DoubleCartEntry implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
