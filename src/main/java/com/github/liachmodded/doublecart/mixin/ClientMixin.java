abe804f814fceb0cb512c179a2e72b3c3b1cf199
liach <liach@users.noreply.github.com>
2020-05-14 21:00:22 -0500

    Update
    Signed-off-by: liach <liach@users.noreply.github.com>

---
+++ src/main/java/com/github/liachmodded/doublecart/mixin/ClientMixin.java
@@ -0,0 +1,15 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package com.github.liachmodded.doublecart.mixin;
+
+import net.minecraft.client.MinecraftClient;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftClient.class)
+public abstract class ClientMixin {
+	// placeholder
+}
